1925abc82041271b7d954d6e2646ca89e7bc20f1
Michael Gierasimiuk <michael@gierasimiuk.com>
2019-04-05 19:58:19 +1100

    Adding skeleton project

---
+++ src/main/java/com/gierasimiuk/jwtrest/JwtrestApplication.java
@@ -0,0 +1,13 @@
+package com.gierasimiuk.jwtrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtrestApplication.class, args);
+	}
+
+}
